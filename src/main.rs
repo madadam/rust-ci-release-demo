de8619e0ad68fd35435c1b19993f968569090273
Adam Cigánek <adam.ciganek@gmail.com>
2016-04-25 15:15:16 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("{}", hello_world());
+}
+
+fn hello_world() -> &'static str {
+    "Hello world"
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(hello_world(), "Hello world");
+    }
+}
