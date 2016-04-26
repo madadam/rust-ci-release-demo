40222e1386b83474ff626f8ffc252eb99a0f4e32
Adam Cigánek <adam.ciganek@gmail.com>
2016-04-26 16:58:13 +0200

    Version change to 0.0.52

--- ci/appveyor/before_deploy.ps1
+++ ci/appveyor/before_deploy.ps1
@@ -1,4 +1,4 @@
-$NAME = "$env:PROJECT_NAME-v$env:VERSION-$env:PLATFORM"
+$NAME = "$env:PROJECT_NAME-v$env:PROJECT_VERSION-$env:PLATFORM"
 
 cargo build --release
 
@@ -7,10 +7,10 @@ git config --global user.email adam.ciganek@gmail.com
 git config --global user.name madadam
 git fetch --tags
 
-if (git tag -l "$env:VERSION") {
-  echo "TODO: create and push tag $env:VERSION"
-  # git tag $env:VERSION -am "Version $env:VERSION" $APPVEYOR_REPO_COMMIT
-  # git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_NAME} tag $env:VERSION > /dev/null 2>&1
+if (git tag -l "$env:PROJECT_VERSION") {
+  echo "TODO: create and push tag $env:PROJECT_VERSION"
+  # git tag $env:PROJECT_VERSION -am "Version $env:PROJECT_VERSION" $APPVEYOR_REPO_COMMIT
+  # git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_NAME} tag $env:PROJECT_VERSION > /dev/null 2>&1
 }
 
 New-Item -ItemType directory -Path staging
@@ -20,6 +20,4 @@ Copy-Item installer\bundle\* staging\$NAME
 
 cd staging
 7z a ../$NAME.zip *
-cd ..
-
-appveyor PushArtifact $NAME.zip
+Push-AppveyorArtifact ../$NAME.zip
