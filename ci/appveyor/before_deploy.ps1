e10e095586350158610f2f74cd8d1d800afd5a88
Adam Cigánek <adam.ciganek@gmail.com>
2016-04-26 16:33:42 +0200

    Version change to 0.0.50

--- ci/appveyor/before_deploy.ps1
+++ ci/appveyor/before_deploy.ps1
@@ -2,6 +2,17 @@ $NAME = "$PROJECT_NAME-v$VERSION-$PLATFORM"
 
 cargo build --release
 
+# Tag this commit if not already tagged.
+git config --global user.email adam.ciganek@gmail.com
+git config --global user.name madadam
+git fetch --tags
+
+if (git tag -l "$VERSION") {
+  echo "TODO: create and push tag $VERSION"
+  # git tag $VERSION -am "Version $VERSION" $TRAVIS_COMMIT
+  # git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} tag $VERSION > /dev/null 2>&1
+}
+
 New-Item -ItemType directory -Path staging
 New-Item -ItemType directory -Path staging\$NAME
 Copy-Item target\release\$PROJECT_NAME.exe staging\$NAME
