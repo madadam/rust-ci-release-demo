81f2763f4b1c48981f2fca02d70b14b8a21da4d5
Adam Cigánek <adam.ciganek@gmail.com>
2016-04-26 16:45:19 +0200

    Version change to 0.0.51

--- ci/appveyor/before_deploy.ps1
+++ ci/appveyor/before_deploy.ps1
@@ -1,4 +1,4 @@
-$NAME = "$PROJECT_NAME-v$VERSION-$PLATFORM"
+$NAME = "$env:PROJECT_NAME-v$env:VERSION-$env:PLATFORM"
 
 cargo build --release
 
@@ -7,15 +7,15 @@ git config --global user.email adam.ciganek@gmail.com
 git config --global user.name madadam
 git fetch --tags
 
-if (git tag -l "$VERSION") {
-  echo "TODO: create and push tag $VERSION"
-  # git tag $VERSION -am "Version $VERSION" $TRAVIS_COMMIT
-  # git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} tag $VERSION > /dev/null 2>&1
+if (git tag -l "$env:VERSION") {
+  echo "TODO: create and push tag $env:VERSION"
+  # git tag $env:VERSION -am "Version $env:VERSION" $APPVEYOR_REPO_COMMIT
+  # git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_NAME} tag $env:VERSION > /dev/null 2>&1
 }
 
 New-Item -ItemType directory -Path staging
 New-Item -ItemType directory -Path staging\$NAME
-Copy-Item target\release\$PROJECT_NAME.exe staging\$NAME
+Copy-Item target\release\$env:PROJECT_NAME.exe staging\$NAME
 Copy-Item installer\bundle\* staging\$NAME
 
 cd staging
